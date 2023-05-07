34187aa64222cc96354330ed49910e8b3eace224
Tabhi109 <tabhinav109@gmail.com>
2023-05-07 16:19:53 +0530

    changes updated !

--- app/static/script/modal.js
+++ app/static/script/modal.js
@@ -63,9 +63,45 @@ $(document).ready(function () {
         })
        
     });
-    
 
-   
+    $('#edit-task-btn').click(function () {
+        const edit = $(this);
+        const id = edit.data('id');
+      
+        // Retrieve the existing content from the database using a SELECT statement
+        $.ajax({
+          type: 'GET',
+          url: '/get-content/' + id,
+          success: function (res) {
+            // Display the existing content in a form that allows the user to make changes
+            $('#edit-modal').find('.form-control').val(res.content);
+          },
+          error: function () {
+            console.log('Error');
+          }
+        });
+      });
+
+      $('#edit-task-btn').click(function () {
+        const tID = $('#task-form-display').attr('taskID');
+        const content = $('#edit-modal').find('.form-control').val();
+      
+        $.ajax({
+          type: 'PUT',
+          url: '/update/' + tID,
+          contentType: 'application/json;charset=UTF-8',
+          data: JSON.stringify({
+            'content': content
+          }),
+          success: function (res) {
+            console.log(res.response);
+            location.reload();
+          },
+          error: function () {
+            console.log('Error');
+          }
+        });
+      });
     
     
     $('.remove').click(function () {
@@ -91,6 +127,9 @@ $(document).ready(function () {
 
         if (state.text() === "Todo") {
             new_state = "In Progress"
+        }
+        if (state.text() === "In Progress") {
+            new_state = "Completed"
         }
        
         console.log(new_state)
